7cb5f61e3e4faa47b09f4d9c65bbdc662b17419b
Александр Крюков <dondarubin@gmail.com>
2024-06-24 17:47:06 +0400

    add alias

--- tests/lib/rules/fsd-check-path.js
+++ tests/lib/rules/fsd-check-path.js
@@ -3,16 +3,33 @@
 const rule = require("../../../lib/rules/fsd-check-path"),
   RuleTester = require("eslint").RuleTester;
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {ecmaVersion: 6, sourceType: 'module'}
+});
 ruleTester.run("fsd-check-path", rule, {
   valid: [
-
+    {
+      filename: 'C:\\Users\\ulbi-project\\src\\entities\\Article',
+      code: "import { SomeActions, SomeReducer } from '../../model/slices/SomeSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "error",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Users\\ulbi-project\\src\\entities\\Article',
+      code: "import { SomeActions, SomeReducer } from 'entities/Article/model/slices/SomeSlice'",
+      errors: [{ message: "Within a current slice, the paths must be relative" }],
+    },
+    {
+      filename: 'C:\\Users\\ulbi-project\\src\\entities\\Article',
+      code: "import { SomeActions, SomeReducer } from '@/entities/Article/model/slices/SomeSlice'",
+      errors: [{ message: "Within a current slice, the paths must be relative" }],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
     },
   ],
 });
